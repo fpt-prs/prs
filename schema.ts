b417827106182a5dd95cf94fbea24d735e9746dc
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2023-10-14 14:49:51 +0700

    fix: fix table field

--- schema.ts
+++ schema.ts
@@ -1,4 +1,4 @@
-import { int, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, serial, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 export const product = mysqlTable("product", {
   id: serial("id").primaryKey(),
@@ -25,11 +25,11 @@ export const collection = mysqlTable("collection", {
 });
 
 export const collection_product = mysqlTable("product_collection", {
-  id: serial("id").primaryKey(),
   collection_id: int("collection_id")
     .notNull()
     .references(() => collection.id),
   product_id: int("product_id")
     .notNull()
     .references(() => product.id),
+  created: timestamp("created").notNull(),
 });
