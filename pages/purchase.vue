171abc631e126cac60be14bfc25a45fbedeab6e9
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-11-03 08:44:51 +0700

    feat: add billing

---
+++ pages/purchase.vue
@@ -0,0 +1,30 @@
+<template>
+  <UButton
+    class="absolute top-5 left-5"
+    color="gray"
+    variant="ghost"
+    label="Back"
+    icon="i-heroicons-arrow-left"
+    @click="$router.back()"
+  />
+
+  <div class="w-full h-screen flex flex-col gap-5 items-center justify-center">
+    <img
+      src="https://cdn.britannica.com/17/155017-050-9AC96FC8/Example-QR-code.jpg"
+      alt="hero"
+      class="w-52 h-52"
+    />
+
+    <p class="text-xl">Amount: {{ amount }} VND</p>
+
+    <p class="text-2xl">Content: AJVADSAIF</p>
+  </div>
+</template>
+
+<script setup>
+// get query
+const { query } = useRoute();
+const amount = query.amount;
+</script>
+
+<style></style>
---
+++ pages/settings/billing.vue
@@ -0,0 +1,46 @@
+<template>
+  <NuxtLayout name="settings">
+    <div class="grow">
+      <p class="text-xl px-4 py-3">Billing</p>
+      <p class="border-y border-color px-4 py-3">Billing detail</p>
+      <div class="px-4 py-3"></div>
+      <p class="border-y border-color px-4 py-3">Purchase new views</p>
+      <div class="px-4 py-3">
+        <div class="grid grid-cols-3 gap-4">
+          <UCard
+            class="cursor-pointer hover:bg-gray-600"
+            as="a"
+            href="/purchase?amount=50000"
+          >
+            <p>50.000 VND</p>
+            <p class="text-gray-500">20 views</p>
+          </UCard>
+          <UCard
+            class="cursor-pointer bg-gray-600"
+            as="a"
+            href="/purchase?amount=100000"
+          >
+            <p>100.000 VND</p>
+            <p class="text-gray-500">50 views</p>
+          </UCard>
+          <UCard
+            class="cursor-pointer bg-gray-600"
+            as="a"
+            href="/purchase?amount=200000"
+          >
+            <p>200.000 VND</p>
+            <p class="text-gray-500">120 views</p>
+          </UCard>
+        </div>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+useHead({
+  title: "Billing",
+});
+</script>
+
+<style></style>
