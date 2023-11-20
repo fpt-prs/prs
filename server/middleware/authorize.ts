b7904bae8d6018037cfd4a293be6dd0f1d80f831
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-11-20 22:29:04 +0700

    feat: api auth

---
+++ server/middleware/authorize.ts
@@ -0,0 +1,47 @@
+import { getServerSession } from "#auth";
+
+const apiMap: any = {
+  "/api/bills": "payment",
+  "/api/collection": "collection",
+  "/api/notifications": "notification",
+  "/api/permissions": "permission",
+  "/api/pricing-options": "payment",
+  "/api/products": "product",
+  "/api/roles": "role",
+  "/api/users": "user",
+};
+
+export default defineEventHandler(async (event) => {
+  const route = event.path;
+  const includedPaths = Object.keys(apiMap);
+  if (!includedPaths.includes(route)) {
+    return;
+  }
+
+  const session = await getServerSession(event);
+  const user = session?.user;
+  const roles = (user as any)?.roles;
+  const permissions = roles
+    .map((r: any) => r?.permissions)
+    .flat()
+    .map((p: any) => p?.name);
+
+  const method = event.method;
+  let actionPer = "";
+  switch (method) {
+    case "GET":
+      actionPer = "read";
+      break;
+    default:
+      actionPer = "write";
+      break;
+  }
+  const neededPermissions = `${apiMap[route]}.${actionPer}.all`;
+  const hasPermission = permissions.includes(neededPermissions);
+
+  if (!hasPermission) {
+    return sendError(event, createError({ statusCode: 403 }));
+  }
+
+  console.log("route", route);
+});
